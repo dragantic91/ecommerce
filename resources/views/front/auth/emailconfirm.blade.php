456eb83364802b6db1ec98bf8e38d0b5eb8b7ab7
dragantic91 <dragantic91@gmail.com>
2018-01-18 13:00:23 +0100

    Translate

--- resources/views/front/auth/emailconfirm.blade.php
+++ resources/views/front/auth/emailconfirm.blade.php
@@ -4,9 +4,9 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Registration Confirmed</div>
+                    <div class="panel-heading">Registrierung erfolgreich!</div>
                     <div class="panel-body">
-                        Your Email is successfully verified. Click here to <a href="{{url('/login')}}">login</a>
+                        Ihre Email wurde erfolgreich verifiziert.
                     </div>
                 </div>
             </div>
--- resources/views/front/emails/confirmation.blade.php
+++ resources/views/front/emails/confirmation.blade.php
@@ -1,14 +1,13 @@
 <p style="padding: 20px;">
-    Hi,<br>
+    Hallo,<br>
 
-    Please verify your email address to complete your Schoengebraucht Account.<br>
+    bitte verifizieren Sie Ihre Emailadresse, um Ihr Profil zu vervollständigen.<br>
 </p>
 <p style="padding: 20px;">
-    <a href="{{url('/verifyemail/'.$token)}}" target="_blank" style="text-decoration: none; padding: 20px; height: 50px; width: 260px; background-color: #1f4f82; color: #ffffff;">Verify Email Address</a>
+    <a href="{{url('/verifyemail/'.$token)}}" target="_blank" style="text-decoration: none; padding: 20px; height: 50px; width: 260px; background-color: #1f4f82; color: #ffffff;">Emailadresse verifizieren</a>
 </p>
 <p style="padding: 20px;">
-    Thank you,<br>
-    The Schoengebraucht Account Team
+    Danke.<br>
 </p>
 
 
