613ba771e90779440f43ce42af2b26246f1b3b97
Jovan Ivanovic <jovanivanovic@einutech.com>
2018-01-26 14:49:04 +0100

    CSV Newsletter Download

--- app/Http/Controllers/Admin/NewsletterController.php
+++ app/Http/Controllers/Admin/NewsletterController.php
@@ -10,7 +10,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\DataGrid\Facade as DataGrid;
 use App\Models\Database\Subscriber;
-use PDF;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Illuminate\Http\Request;
 
 class NewsletterController extends AdminController
@@ -47,8 +49,46 @@ class NewsletterController extends AdminController
         view()->share('subscribers', $subscribers);
 
         if ($request->has('download')) {
-            $pdf = PDF::loadView('admin.newsletter.pdfview');
-            return $pdf->download('pdfview.pdf');
+            $spreadsheet = new Spreadsheet();
+
+            $title = 'Newsletter';
+            
+            $spreadsheet->getProperties()->setCreator('Webtory')
+                ->setLastModifiedBy('Webtory')
+                ->setTitle($title)
+                ->setSubject($title)
+                ->setDescription($title)
+                ->setCategory($title);
+
+            $spreadsheet->setActiveSheetIndex(0);
+            $sheet = $spreadsheet->getActiveSheet();
+
+
+            $sheet->setCellValue('A1', 'ID')
+                ->setCellValue('B1', 'Email');
+
+            $count = 1;
+            foreach ($subscribers as $subscriber) {
+                $count++;
+                $sheet->setCellValue('A'.$count, $subscriber->id)
+                    ->setCellValue('B'.$count, $subscriber->email);
+            }
+
+            $sheet->setTitle($title);
+
+            header('Content-Type: text/csv');
+            header('Content-Disposition: attachment;filename="'.$title.'.csv"');
+            header('Cache-Control: max-age=0');
+            header('Cache-Control: max-age=1');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+            header('Cache-Control: cache, must-revalidate');
+            header('Pragma: public');
+
+            $writer = IOFactory::createWriter($spreadsheet, 'Csv');
+            $writer->setDelimiter(';');
+            $writer->setEnclosure('');
+            $writer->save('php://output');
         }
 
         return view('admin.newsletter.pdfview');
